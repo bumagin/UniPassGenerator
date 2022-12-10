998a4bce7abeabb73356f1d6d1fd10e91b2dc4ae
wexor <wexor@VADIM>
2022-12-10 20:17:00 +0300

    Добавьте файлы проекта.

---
+++ passGenerator/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace passGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.generate = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.copy = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 46);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Здесь будет ваш пароль!";
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(21, 213);
+            this.generate.Margin = new System.Windows.Forms.Padding(4);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(385, 32);
+            this.generate.TabIndex = 1;
+            this.generate.Text = "Сгенерировать";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 110);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Заглавные буквы";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(21, 132);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(171, 20);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Нетипичные символы";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.copy);
+            this.groupBox1.Controls.Add(this.info);
+            this.groupBox1.Controls.Add(this.save);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.generate);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(431, 384);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Меню";
+            // 
+            // copy
+            // 
+            this.copy.Location = new System.Drawing.Point(21, 252);
+            this.copy.Margin = new System.Windows.Forms.Padding(4);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(385, 32);
+            this.copy.TabIndex = 9;
+            this.copy.Text = "Скопировать пароль";
+            this.copy.UseVisualStyleBackColor = true;
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // info
+            // 
+            this.info.Location = new System.Drawing.Point(21, 331);
+            this.info.Margin = new System.Windows.Forms.Padding(4);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(385, 32);
+            this.info.TabIndex = 8;
+            this.info.Text = "Информация";
+            this.info.UseVisualStyleBackColor = true;
+            this.info.Click += new System.EventHandler(this.info_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(21, 292);
+            this.save.Margin = new System.Windows.Forms.Padding(4);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(385, 32);
+            this.save.TabIndex = 7;
+            this.save.Text = "Сохранить пароль на ПК";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 78);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(384, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Назначение пароля";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 161);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество символов";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(21, 181);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(385, 22);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 414);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Генератор паролей";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button info;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button copy;
+    }
+}
+
---
+++ passGenerator/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string pass;
+        string description = "";
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            pass = "";
+
+            char[] symbols = { 'a', 'b', 'с', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z',/*25*/ '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'С', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '%', '?', '/' };
+
+            Random rndm = new Random();
+
+            int indx;
+
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                if (checkBox1.Checked == false && checkBox2.Checked == false)
+                {
+                    indx = rndm.Next(0, 34);
+                    pass = pass + symbols[indx];
+                }
+
+                if (checkBox1.Checked == true && checkBox2.Checked == false)
+                {
+                    indx = rndm.Next(0, 59);
+                    pass = pass + symbols[indx];
+                }
+
+                if (checkBox1.Checked == false && checkBox2.Checked == true)
+                {
+                    indx = rndm.Next(0, 63);
+
+                    if (indx <= 34 || indx >= 61)
+                    {
+                        pass = pass + symbols[indx];
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+
+                if (checkBox1.Checked == true && checkBox2.Checked == true)
+                {
+                    indx = rndm.Next(0, 63);
+                    pass = pass + symbols[indx];
+                }
+            }
+            textBox1.Text = pass;
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа создана для генерации паролей разных видов.\n" +
+                "Разработка - github.com/bumagin\n" +
+                "Версия программы - 0.2", "Информация");
+        }
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(pass);
+            MessageBox.Show("Пароль скопирован в ваш буфер обмена!", "Успех!");
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            description = textBox2.Text;
+
+            string date = Convert.ToString(DateTime.Now);
+
+            System.IO.File.WriteAllText("password.txt", description + "\n" + pass + "\n" + date);
+
+            MessageBox.Show("Пароль успешно сохранен в файле password.txt", "Успех!");
+        }
+    }
+}
